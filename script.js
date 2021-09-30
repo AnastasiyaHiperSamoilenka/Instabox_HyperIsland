5836352286c71e67c7c31daccb24d350e529b271
AnastasiyaSam <51028731+AnastasiyaSamoilenka@users.noreply.github.com>
2021-09-30 21:14:33 +0200

    Cleaning of code, changing names of variables and functions, shaking box animation

--- script.js
+++ script.js
@@ -1,100 +1,94 @@
-//Work with Json
-
 //Here we are fetching deliveryContent.json file and execute the json() function which also returns a promise and runs then* function. 
-//Finaly we get actual Json data as a parameter.
+//Finally we get the actual Json data as a parameter.
 fetch('deliveryContent.json')
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    //Here we create the code which will append the data to our page.
+    //Here we call a function which will append the data to our page.
     appendData(data);
   })
+
   .catch(function (err) {
     console.log('error: ' + err);
   });
 
-
 //Create array of objects 
-
-let backetArray = [];
+let ArrayOfCartItems = [];
 
 // //Link to the session storage
 // let sessionString = sessionStorage.getItem('TheArray');
 // if (sessionString) {
-//   backetArray = JSON.parse(sessionString);
+//   ArrayOfCartItems = JSON.parse(sessionString);
 
 //   // Add sum of clicked items to the cart btn
-//   let btnBacketPosition = document.getElementsByClassName("btnCartPrice")[0];
+//   let Cart_btn = document.getElementsByClassName("btnCartPrice")[0];
 
 //   let sumOnCart = 0;
-//   for (let i = 0; i < backetArray.length; i++) {
-//     sumOnCart += backetArray[i].price;
+//   for (let i = 0; i < ArrayOfCartItems.length; i++) {
+//     sumOnCart += ArrayOfCartItems[i].price;
 //   }
 
 
-//   btnBacketPosition.innerHTML = sumOnCart;
+//   Cart_btn.innerHTML = sumOnCart;
 // }
 
-
-
 function appendData(data) {
-  let mainContainer = document.getElementsByClassName("grid-container")[0];
+  let mainGridContainer = document.getElementsByClassName("grid-container")[0];
 
   data.forEach(element => {
-    //Add diw which is Parent container to each box
+    //Add div which is Parent container to each box
     let boxDiv = document.createElement("div");
     boxDiv.classList.add('box');
 
     //Add div which is front door
-    let gridItemDiv = document.createElement("div");
-    gridItemDiv.classList.add('grid-item');
-    gridItemDiv.innerHTML = element.DeliveryOption + "</br>" + element.price + " " + element.currency;
+    let box__firstDiv = document.createElement("div");
+    box__firstDiv.classList.add('box__first');
+    box__firstDiv.innerHTML = element.DeliveryOption + "</br>" + element.price + " " + element.currency;
 
 
 
     //Add div which is hidden box
-    let hiddenText__and_positionDiv = document.createElement("div");
-    hiddenText__and_positionDiv.classList.add('hiddenText__and_position');
+    let box__secondDiv = document.createElement("div");
+    box__secondDiv.classList.add('box__second');
 
     //Add to the HTML <a> tag (button)to the hidden box with link
-    let a__HiddenText__and_positionDiv = document.createElement("a");
-    a__HiddenText__and_positionDiv.href = "#";
-    hiddenText__and_positionDiv.appendChild(a__HiddenText__and_positionDiv);
+    let a__box__secondDiv = document.createElement("a");
+    a__box__secondDiv.href = "#";
+    box__secondDiv.appendChild(a__box__secondDiv);
 
 
     //Create p tag and append text to it
     let pTag = document.createElement("p");
     pTag.classList.add('btn_p');
-    a__HiddenText__and_positionDiv.appendChild(pTag);
+    a__box__secondDiv.appendChild(pTag);
     pTag.innerHTML = element.InnerButtonText;
 
 
-    //Add items to the backet
-    pTag.addEventListener("click", function addToBacket() {
-      backetArray.push({
+    //Add items to the Cart
+    pTag.addEventListener("click", function addToCart() {
+
+      ArrayOfCartItems.push({
         "DeliveryOption": element.DeliveryOption,
         "price": element.price,
         "currency": element.currency
       });
 
-
       // Add sum of clicked items to the cart btn
-      let btnBacketPosition = document.getElementsByClassName("btnCartPrice")[0];
+      let Cart_btn = document.getElementsByClassName("btnCartPrice")[0];
 
       let sumOnCart = 0;
-      for (let i = 0; i < backetArray.length; i++) {
-        sumOnCart += backetArray[i].price;
+      for (let i = 0; i < ArrayOfCartItems.length; i++) {
+        sumOnCart += ArrayOfCartItems[i].price;
       }
 
-
-      btnBacketPosition.innerHTML = sumOnCart;
+      Cart_btn.innerHTML = sumOnCart;
     });
 
 
-    //Add grid-item div and hiddenText div to the box which is their container
-    boxDiv.appendChild(gridItemDiv);
-    boxDiv.appendChild(hiddenText__and_positionDiv);
+    //Add box__first div and box__second div to the box which is their container
+    boxDiv.appendChild(box__firstDiv);
+    boxDiv.appendChild(box__secondDiv);
 
     //Make door rotation by changing class on click
     boxDiv.addEventListener("click", function toggleDoor() {
@@ -103,15 +97,17 @@ function appendData(data) {
 
 
     //Add all divs to main container
-    mainContainer.appendChild(boxDiv);
+    mainGridContainer.appendChild(boxDiv);
   });
 }
 
 
 //Making item button working
-document.getElementsByClassName('btn-backet')[0].onclick = function () {
-  sessionStorage.setItem('TheArray', JSON.stringify(backetArray));
+document.getElementsByClassName('Cart_btn')[0].onclick = function () {
+  //Session storage  method called to store values of Cart items from the array in the session storage
+  sessionStorage.setItem('TheArray', JSON.stringify(ArrayOfCartItems));
 
+  // redirect us to the second page whet Cart button is clicked
   window.location.href = '/page2.html';
 };
 
